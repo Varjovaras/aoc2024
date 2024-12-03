768c52fe511e83c653fea91ffcb799ef7e31f91c
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-12-03 12:35:28 +0200

    day1 solution 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,44 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, ali!");
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let vec: Vec<&str> = contents.split("\n").collect();
+    let vec_tuples: Vec<(i32, i32)> = vec
+        .iter()
+        .filter_map(|s| {
+            let parts: Vec<&str> = s.split_whitespace().collect();
+            if parts.len() >= 2 {
+                Some((parts[0].parse().ok()?, parts[1].parse().ok()?))
+            } else {
+                None
+            }
+        })
+        .collect();
+    let mut vec1: Vec<i32> = vec![];
+    let mut vec2: Vec<i32> = vec![];
+
+    for (first, second) in vec_tuples {
+        vec1.push(first);
+        vec2.push(second);
+    }
+
+    vec1.sort();
+    vec2.sort();
+
+    let vec_tuples: Vec<(i32, i32)> = vec1
+        .iter()
+        .zip(vec2.iter())
+        .map(|(a, b)| (*a, *b))
+        .collect();
+
+    let mut total = 0;
+    for tuple in vec_tuples {
+        total += tuple.0.abs_diff(tuple.1);
+    }
+
+    dbg!(total);
 }
