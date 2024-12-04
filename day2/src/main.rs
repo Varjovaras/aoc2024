d7e342056280138f681d9e4474f361c39f78ed79
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-12-04 16:01:53 +0200

    day3 solution 1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let file_path = &args[1];
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-    let report_vectors: Vec<Vec<i32>> = contents
+    let reports: Vec<Vec<i32>> = contents
         .lines()
         .map(|line| {
             line.split_whitespace()
@@ -16,33 +16,45 @@ fn main() {
         .collect();
 
     let mut total_safe_reports = 0;
-    for report in report_vectors {
-        let mut safe = true;
-        let first_direction = check_dir(report[0] - report[1]);
-
-        for (index, _) in report.iter().enumerate().take(report.len() - 1) {
-            let distance = report[index] - report[index + 1];
-            if distance == 0 {
-                safe = false;
-                break;
-            }
-            if distance.abs() > 3 || distance.abs() < 1 {
-                safe = false;
-                break;
-            }
-            if check_dir(report[index] - report[index + 1]) != first_direction {
-                safe = false;
-                break;
-            }
-        }
-        if safe {
+    for report in reports {
+        if is_report_safe(&report) || is_report_fixable(&report) {
             total_safe_reports += 1;
         }
     }
 
-    println!("{}", total_safe_reports)
+    println!("{}", total_safe_reports);
 }
 
-fn check_dir(i: i32) -> bool {
-    i > 0
+fn is_report_safe(report: &[i32]) -> bool {
+    let first_direction = report[0] - report[1] > 0;
+
+    for (index, _) in report.iter().enumerate().take(report.len() - 1) {
+        let distance = report[index] - report[index + 1];
+        if distance == 0 {
+            return false;
+        }
+        if distance.abs() > 3 || distance.abs() < 1 {
+            return false;
+        }
+        if (report[index] - report[index + 1] > 0) != first_direction {
+            return false;
+        }
+    }
+    true
+}
+
+fn is_report_fixable(report: &[i32]) -> bool {
+    for i in 0..report.len() {
+        // Create a new vector without the element at index i
+        let potentially_fixed_report: Vec<i32> = report
+            .iter()
+            .enumerate()
+            .filter(|&(index, _)| index != i)
+            .map(|(_, &val)| val)
+            .collect();
+        if is_report_safe(&potentially_fixed_report) {
+            return true;
+        }
+    }
+    false
 }
---
+++ day3/src/main.rs
@@ -0,0 +1,45 @@
+use std::{char, env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let chars: Vec<char> = contents.chars().collect();
+    let mut total = 0;
+    for (index, char) in chars.iter().enumerate() {
+        if char == &'m'
+            && chars.get(index + 1) == Some(&'u')
+            && chars.get(index + 2) == Some(&'l')
+            && chars.get(index + 3) == Some(&'(')
+        {
+            let mut first_number_as_string = String::new();
+            let mut second_number_as_string = String::new();
+            let mut check_second_number = false;
+            let mut current_index = index + 4;
+
+            while let Some(next_char) = chars.get(current_index) {
+                match next_char {
+                    c if c.is_ascii_digit() && !check_second_number => {
+                        first_number_as_string.push(*c);
+                    }
+                    c if c.is_ascii_digit() && check_second_number => {
+                        second_number_as_string.push(*c);
+                    }
+                    &',' if !first_number_as_string.is_empty() => {
+                        check_second_number = true;
+                    }
+                    &')' if check_second_number && !second_number_as_string.is_empty() => {
+                        let mul = first_number_as_string.parse::<i32>().unwrap()
+                            * second_number_as_string.parse::<i32>().unwrap();
+                        total += mul;
+                        break;
+                    }
+                    _ => break,
+                }
+                current_index += 1;
+            }
+        }
+    }
+    println!("{}", total);
+}
