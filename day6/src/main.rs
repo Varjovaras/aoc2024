7fc47b5037fa41d5b7f23354496707a161be9923
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-12-09 23:34:01 +0200

    day6 uncompleted version

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,18 +1,14 @@
-use file_reader::read_text_file;
+#![allow(unused_variables, unused_mut)]
 
-enum Direction {
-    Up,
-    Down,
-    Left,
-    Right,
-}
+use file_reader::read_text_file;
 
 fn main() {
     let contents = read_text_file();
-    let rows: Vec<Vec<char>> = contents
+    let mut rows: Vec<Vec<char>> = contents
         .lines()
         .map(|line| line.chars().collect())
         .collect();
+
     let start_sq: Option<(usize, usize)> = rows.iter().enumerate().find_map(|(i, row)| {
         row.iter().enumerate().find_map(
             |(j, char)| {
@@ -25,7 +21,45 @@ fn main() {
         )
     });
 
-    println!("Hello, world!");
+    let (mut x, mut y) = start_sq.unwrap();
+
+    loop {
+        move_arrow(x, y, &rows);
+        if next_move_is_out_of_bounds(x, y, &rows) {
+            break;
+        }
+    }
+}
+
+fn next_move_is_out_of_bounds(x: usize, y: usize, rows: &[Vec<char>]) -> bool {
+    todo!()
+}
+
+fn move_arrow(x: usize, y: usize, rows: &[Vec<char>]) {
+    let char = rows[x][y];
+    match char {
+        '^' => try_to_move_up(),
+        '>' => try_to_move_right(),
+        'v' => try_to_move_down(),
+        '<' => try_to_move_left(),
+        _ => panic!("how??"),
+    }
+}
+
+fn try_to_move_left() {
+    todo!()
+}
+
+fn try_to_move_right() {
+    todo!()
+}
+
+fn try_to_move_down() {
+    todo!()
+}
+
+fn try_to_move_up() {
+    todo!()
 }
 
 fn is_guard(char: &char) -> bool {
