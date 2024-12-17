b50719bd457f8a6b5d4ea829307e3ec0d81510ce
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-12-17 07:48:10 +0200

    day8 solution 2

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -31,12 +31,15 @@ fn calculate_antinodes(
     grid: &[Vec<char>],
     radars_checked: &HashSet<char>,
 ) -> Vec<(usize, usize)> {
-    let char = grid[start_position.0][start_position.1];
-    if char == '.' || char == '#' || radars_checked.contains(&char) {
+    let current_character = grid[start_position.0][start_position.1];
+    if current_character == '.'
+        || current_character == '#'
+        || radars_checked.contains(&current_character)
+    {
         return vec![];
     }
 
-    let matching_characters = find_matching_characters(grid, char);
+    let matching_characters = find_matching_characters(grid, current_character);
     location_of_antinodes(&matching_characters, grid)
 }
 
@@ -56,40 +59,38 @@ fn location_of_antinodes(locations: &[(usize, usize)], grid: &[Vec<char>]) -> Ve
     let mut antinode_locations = vec![];
     let x_len = grid.len() as isize;
     let y_len = grid[0].len() as isize;
+
     for i in 0..locations.len() {
         for j in i + 1..locations.len() {
-            let first_location = (locations[i].0 as isize, locations[i].1 as isize);
-            let second_location = (locations[j].0 as isize, locations[j].1 as isize);
+            let first_antenna = (locations[i].0 as isize, locations[i].1 as isize);
+            let second_antenna = (locations[j].0 as isize, locations[j].1 as isize);
 
-            let diff_x = first_location.0 - second_location.0;
-            let diff_y = first_location.1 - second_location.1;
+            let diff_x = second_antenna.0 - first_antenna.0;
+            let diff_y = second_antenna.1 - first_antenna.1;
 
-            if first_location.0 + diff_x >= 0
-                && first_location.0 + diff_x < x_len
-                && first_location.1 + diff_y >= 0
-                && first_location.1 + diff_y < y_len
-            {
-                antinode_locations.push((
-                    (first_location.0 + diff_x) as usize,
-                    (first_location.1 + diff_y) as usize,
-                ));
+            let mut current_x = first_antenna.0;
+            let mut current_y = first_antenna.1;
+
+            while current_x >= 0 && current_x < x_len && current_y >= 0 && current_y < y_len {
+                antinode_locations.push((current_x as usize, current_y as usize));
+
+                current_x += diff_x;
+                current_y += diff_y;
             }
 
-            if second_location.0 - diff_x >= 0
-                && second_location.0 - diff_x < x_len
-                && second_location.1 - diff_y >= 0
-                && second_location.1 - diff_y < y_len
-            {
-                antinode_locations.push((
-                    (second_location.0 - diff_x) as usize,
-                    (second_location.1 - diff_y) as usize,
-                ));
+            current_x = second_antenna.0;
+            current_y = second_antenna.1;
+
+            while current_x >= 0 && current_x < x_len && current_y >= 0 && current_y < y_len {
+                antinode_locations.push((current_x as usize, current_y as usize));
+
+                current_x -= diff_x;
+                current_y -= diff_y;
             }
         }
     }
 
     antinode_locations.sort_unstable();
     antinode_locations.dedup();
-
     antinode_locations
 }
